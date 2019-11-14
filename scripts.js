f54ec3527c69835c6515520c32a0fa2f3944769d
fas19 <47185536+fas19@users.noreply.github.com>
2019-11-14 17:08:40 +0000

    klárað að laga eslint villur

--- scripts.js
+++ scripts.js
@@ -1,30 +1,103 @@
-const API_URL = 'https://apis.is/company?name=';
-document.addEventListener('DOMContentLoaded', function () {
-  const comp = document.querySelector('.companies');
-  program.init(comp);
-
-});
-
 
 /**
  * Leit að fyrirtækjum á Íslandi gegnum apis.is
  */
+const API_URL = 'https://apis.is/company?name=';
 
 const program = (() => {
   let companiesSection;
   let container;
   let form;
 
-  function init(_companies) {
-    companiesSection = _companies;
-
-
-    container = companiesSection.querySelector('.results');
-
-    form = companiesSection.querySelector('form');
-    form.addEventListener('submit', onSubmit);
-
+  function el(name, ...children) {
+    const element = document.createElement(name);
+    for (const child of children) {  /* eslint-disable-line */
+      if (typeof child === 'string') {
+        element.appendChild(document.createTextNode(child));
+      } else {
+        element.appendChild(child);
+      }
+    }
+    return element;
   }
+
+  function empty(element) {
+    while (element.firstChild) {
+      element.removeChild(element.firstChild);
+    }
+  }
+
+  function displayError(error) {
+    empty(container);
+    const div = el('div',
+      el('dl',
+        el('dd', `${error}`)));
+    container.appendChild(div);
+  }
+
+  function displayLoading() {
+    empty(container);
+    const div = el('div',
+      el('img'),
+      el('p', 'Leita að fyrirtækjum...'));
+    div.classList.add('loading');
+    div.firstChild.setAttribute('src', 'loading.gif');
+
+    container.append(div);
+  }
+
+  function show(data) {
+    if (data.results.length === 0) {
+      displayError('Ekkert fyrirtæki fannst fyrir leitarstreng');
+      return;
+    }
+    const { results } = data;
+    empty(container);
+    for (const result of results) {     /* eslint-disable-line */
+      const active = parseInt(result.active, 10);
+
+      if (active === 0) {
+        const div = el('div',
+          el('dl',
+            el('dt', 'Nafn'),
+            el('dd', `${result.name}`),
+            el('dt', 'Kennitala'),
+            el('dd', `${result.sn}`)));
+        div.classList.add('company');
+        div.classList.add('company--inactive');
+        container.appendChild(div);
+      } else {
+        const div = el('div',
+          el('dl',
+            el('dt', 'Nafn'),
+            el('dd', `${result.name}`),
+            el('dt', 'Kennitala'),
+            el('dd', `${result.sn}`),
+            el('dt', 'Heimilisfang'),
+            el('dd', `${result.address}`)));
+        div.classList.add('company');
+        div.classList.add('company--active');
+        container.appendChild(div);
+      }
+    }
+  }
+
+  function getData(data) {
+    fetch(API_URL + data)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error();
+      })
+      .then((jsonResponse) => {
+        show(jsonResponse);
+      })
+      .catch(() => {
+        displayError('Villa við að sækja gögn.');
+      });
+  }
+
   function onSubmit(e) {
     e.preventDefault();
     const text = e.target.querySelector('input').value;
@@ -36,108 +109,14 @@ const program = (() => {
       getData(text);
     }
   }
-
-  function getData(data) {
-    fetch(API_URL + data)
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        }
-      })
-      .then(jsonResponse => {
-        show(jsonResponse);
-      })
-      .catch(() => {
-        displayError("Villa við að sækja gögn.");
-      })
+  function init(_companies) {
+    companiesSection = _companies;
 
 
+    container = companiesSection.querySelector('.results');
 
-  }
-
-  function show(data) {
-    if (data.results.length == 0) {
-      displayError('Ekkert fyrirtæki fannst fyrir leitarstreng');
-      return;
-    }
-    results = data.results;
-    empty(container);
-    for (const result of results) {
-      const active = parseInt(result.active);
-
-      if (active == 0) {
-        const div = el('div',
-          el('dl',
-            el('dt', "Nafn"),
-            el('dd', "" + result.name),
-            el('dt', "Kennitala"),
-            el('dd', "" + result.sn),
-          )
-        )
-        div.classList.add("company");
-        div.classList.add("company--inactive");
-        container.appendChild(div);
-      }
-      else {
-        const div = el('div',
-          el('dl',
-            el('dt', "Nafn"),
-            el('dd', "" + result.name),
-            el('dt', "Kennitala"),
-            el('dd', "" + result.sn),
-            el('dt', "Heimilisfang"),
-            el('dd', "" + result.address),
-          )
-        )
-        div.classList.add("company");
-        div.classList.add("company--active");
-        container.appendChild(div);
-
-      }
-
-    }
-  }
-
-  function el(name, ...children) {
-    const element = document.createElement(name);
-    for (const child of children) {
-      if (typeof child === 'string') {
-        element.appendChild(document.createTextNode(child));
-      } else {
-        element.appendChild(child);
-      }
-    }
-    return element;
-  }
-
-  function empty(el) {
-    while (el.firstChild) {
-      el.removeChild(el.firstChild);
-    }
-  }
-
-  function displayError(error) {
-    empty(container);
-    const div = el('div',
-      el('dl',
-        el('dd', "" + error),
-      )
-    )
-    container.appendChild(div);
-
-  }
-
-  function displayLoading() {
-    empty(container);
-    const div = el('div',
-      el('img'),
-      el('p', "Leita að fyrirtækjum...")
-    )
-    div.classList.add("loading");
-    div.firstChild.setAttribute("src", "loading.gif");
-
-    container.append(div);
-
+    form = companiesSection.querySelector('form');
+    form.addEventListener('submit', onSubmit);
   }
 
   return {
@@ -145,3 +124,8 @@ const program = (() => {
   };
 })();
 
+
+document.addEventListener('DOMContentLoaded', () => {
+  const comp = document.querySelector('.companies');
+  program.init(comp);
+});
